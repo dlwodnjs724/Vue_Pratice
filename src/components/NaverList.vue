1c227043eb5d8a6237d7cc7ea70017ff32ea59a7
dlwodnjs724 <dlwodnjs724@naver.com>
2018-08-02 00:25:32 +0900

    second commit

--- src/components/NaverList.vue
+++ src/components/NaverList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout row wrap align-center>
     <v-flex v-for="(item, index) in top">
-      <v-card width="900px" height="40px" style="line-height: 10px;">
+      <v-card width="1000px" height="40px" style="line-height: 10px;">
         <a v-bind:href="item.link" style="text-decoration: none; color: black;" target="_blank"><v-card-text>{{index+1}} {{item.title}}</v-card-text></a>
       </v-card>
     </v-flex>
